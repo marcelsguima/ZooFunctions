a53ed4153fe3fea6ebcb45814fcc3c217b8885e8
marceslguima <marcelsguima@gmail.com>
2022-07-28 19:19:30 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const searchObject = data.species.filter((species) => ids.find((id) => id === species.id));
+  return searchObject;
 }
+
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
