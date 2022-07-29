0cce17c6c961da995104f5ac3e735005f3f0446b
marceslguima <marcelsguima@gmail.com>
2022-07-29 13:35:52 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+  if (employeeName === undefined) return {};
 
+  const searchObject = data.employees.find(
+    (person) => person.firstName === employeeName || person.lastName === employeeName,
+  );
+
+  return searchObject;
+}
+console.log(getEmployeeByName('Orloff'));
 module.exports = getEmployeeByName;
