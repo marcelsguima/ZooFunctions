95bf388abf7bb245e2283c69376e6960e16ccba0
marceslguima <marcelsguima@gmail.com>
2022-07-30 15:14:05 -0300

    Requisito 7

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,13 +1,6 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  if (animal === '') {
-    const count = data.species.forEach(
-      (animal) => {},
-    );
-    return count;
-  }
-}
+function countAnimals(animal) {}
+
 console.log(countAnimals());
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -13,4 +13,21 @@ describe('Testes da função HandlerElephants', () => {
   it('Testa se média da idade dos elefantes está correta', () => {
     expect(handlerElephants('averageAge')).toEqual(10.5);
   });
+
+  it('Testa se o retorno da localização dos elefantes está correto', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+
+  it('Testa se o retorno da localização dos elefantes está correto', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+
+  it('Testa se popularidade dos elefantes está correta', () => {
+    const availability = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(handlerElephants('availability')).toEqual(availability);
+  });
+
+  it('Testa se o retorno da função sem argumento', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
 });
