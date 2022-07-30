3f2789f2f656cc4952eba73eb4e20fdacd606aff
marceslguima <marcelsguima@gmail.com>
2022-07-30 15:01:24 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === '') {
+    const count = data.species.forEach(
+      (animal) => {},
+    );
+    return count;
+  }
 }
-
+console.log(countAnimals());
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,16 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se o retorno da população de elefantes está correta', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  it('Testa se o retorno dos nomes dos elefantes está correto', () => {
+    const actualNames = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(actualNames);
+  });
+
+  it('Testa se média da idade dos elefantes está correta', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
