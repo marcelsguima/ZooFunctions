702b8ec77d88c8757d8055c31ea332c02007e01e
marceslguima <marcelsguima@gmail.com>
2022-08-02 19:22:25 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui oi
+  return entrants.reduce((acc, guest) => {
+    if (guest.age < 18) {
+      acc.child += 1;
+    } else if (guest.age >= 18 && guest.age < 50) {
+      acc.adult += 1;
+    } else { acc.senior += 1; }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) { return 0; }
+  const { child, adult, senior } = countEntrants(entrants);
+  return (child * data.prices.child) + (adult * data.prices.adult) + (senior * data.prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
