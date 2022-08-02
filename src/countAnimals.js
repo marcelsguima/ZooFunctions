1243c0e8ce229983f8ce50712dbd73f5ca115b4a
marceslguima <marcelsguima@gmail.com>
2022-08-02 16:43:37 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {}
+function countAnimals(animal) {
+  if (!animal) {
+    const allAnim = {};
+    data.species.forEach((anim) => { allAnim[anim.name] = anim.residents.length; });
+    return allAnim;
+  }
+  if (animal.sex) {
+    const sex = data.species.find((specie) => specie.name === animal.specie);
+    return sex.residents.filter((animPop) => animPop.sex === animal.sex).length;
+  }
+  if (animal.specie) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+}
+console.log(countAnimals({ specie: 'lions', sex: 'female' }));
 
-console.log(countAnimals());
 module.exports = countAnimals;
--- src/test.js
+++
@@ -1,8 +0,0 @@
-const data = require('../data/zoo_data');
-
-function currentSpecies(animal) {
-  const animalget = data.species.filter(animal);
-  return animalget;
-}
-
-console.log(currentSpecies('lions'));
